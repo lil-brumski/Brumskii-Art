9d5ae19e3f983a28d97d9faa4569608132642331
David Tamaratare Oghenebrume <151965409+lil-brumski@users.noreply.github.com>
2024-04-14 23:11:34 +0100

    Update C.h

--- HEADER_FILES_and_SOURCE_FILE/C.h
+++ HEADER_FILES_and_SOURCE_FILE/C.h
@@ -5,28 +5,28 @@
 #include <iomanip>
 	 
 void C(){
-    int c = 28;
+    int c = 30;
     std::cout<<std::setfill('.');
     
-    std::cout<<std::right<<std::setw(c)<<"......"<<std::left<<std::setw(c)<<"......"<<std::endl;
+    std::cout<<std::right<<std::setw(c)<<"........"<<std::left<<std::setw(c)<<"........"<<std::endl;
     
     //Beginning 
     
-    std::cout<<std::right<<std::setw(c)<<"  *"<<std::left<<std::setw(c)<<"******"<<std::endl;
+    std::cout<<std::right<<std::setw(c)<<"  ******"<<std::left<<std::setw(c)<<"************"<<std::endl;
      
-     std::cout<<std::right<<std::setw(c)<<" **"<<std::left<<std::setw(c)<<"      "<<std::endl;
+     std::cout<<std::right<<std::setw(c)<<" ****"<<std::left<<std::setw(c)<<"              "<<std::endl;
     
     for(int x = 0; x < 5; x++){
-           std::cout<<std::right<<std::setw(c)<<"**"<<std::left<<std::setw(c)<<"       "<<std::endl;
+           std::cout<<std::right<<std::setw(c)<<"****"<<std::left<<std::setw(c)<<"               "<<std::endl;
     }
     
     for(int x = 0; x < 2; x++){
-          std::cout<<std::right<<std::setw(c)<<" **"<<std::left<<std::setw(c)<<"       "<<std::endl;
+          std::cout<<std::right<<std::setw(c)<<" ****"<<std::left<<std::setw(c)<<"               "<<std::endl;
     }
     
-    std::cout<<std::right<<std::setw(c)<<"  *"<<std::left<<std::setw(c)<<"******"<<std::endl;
+    std::cout<<std::right<<std::setw(c)<<"  *******"<<std::left<<std::setw(c)<<"************"<<std::endl;
      
-       std::cout<<std::right<<std::setw(c)<<"......"<<std::left<<std::setw(c)<<"......"<<std::endl;
+       std::cout<<std::right<<std::setw(c)<<"........"<<std::left<<std::setw(c)<<"........"<<std::endl;
     
 }
 
