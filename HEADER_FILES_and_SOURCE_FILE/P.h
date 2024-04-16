c80ec2565c1cef45b327cf86ea58237431cdcd90
David Tamaratare Oghenebrume <151965409+lil-brumski@users.noreply.github.com>
2024-04-16 10:56:26 +0100

    Update P.h

--- HEADER_FILES_and_SOURCE_FILE/P.h
+++ HEADER_FILES_and_SOURCE_FILE/P.h
@@ -15,9 +15,7 @@ void P(){
     for(int x = 0; x < 3; x++){
     std::cout<<std::right<<std::setw(p)<<"**...."<<std::left<<std::setw(p)<<"....**"<<std::endl;
     }
-    
-   // std::cout<<std::right<<std::setw(p)<<"**...."<<std::left<<std::setw(p)<<"....**"<<std::endl;
-    std::cout<<std::right<<std::setw(p)<<"******"<<std::left<<std::setw(p)<<"*****."<<std::endl;
+     std::cout<<std::right<<std::setw(p)<<"******"<<std::left<<std::setw(p)<<"*****."<<std::endl;
     
     for(int x = 0; x < 5; x++){
     std::cout<<std::right<<std::setw(p)<<"**...."<<std::left<<std::setw(p)<<"......"<<std::endl;
