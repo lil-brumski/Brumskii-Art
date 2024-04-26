a9dd96551fde368fbab037d82f43bf234ab06106
David Tamaratare Oghenebrume <151965409+lil-brumski@users.noreply.github.com>
2024-04-26 19:43:21 +0100

    Update main.cpp

--- HEADER_FILES_and_SOURCE_FILE/main.cpp
+++ HEADER_FILES_and_SOURCE_FILE/main.cpp
@@ -25,33 +25,11 @@
 #include "Y.h"
 #include "Z.h"
 #include "SPACE.h"
+#include "boolean_functions.h"
 #include <iostream>
 #include <iomanip>
 #include <string>
 #include <cctype>
-
-
-//Checks if the user's name contains only letters.
-bool isLettersOnly(const std::string& input) {
-    for (size_t i = 0; i < input.length(); ++i) {
-        if (!std::isalpha(input[i]) && input[i] != ' ') {
-            return false;
-        }
-    }
-    return true;
-}
-
-
-//Checks if the user enter two or more consecutive spaces as their name.
-bool hasConsecutiveSpaces(const std::string& str) {
-    for (size_t i = 1; i < str.length(); ++i) {
-        if (std::isspace(str[i]) && std::isspace(str[i - 1])) {
-            return true;
-        }
-    }
-    return false;
-}
-
 
 //Main function where the program starts running.
 int main() {
